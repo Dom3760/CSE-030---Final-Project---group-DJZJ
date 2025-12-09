036027cbbb07ed48d0209eb45ef2a4ed03bc195d
Powerbow <powev0oigaib47@gmail.com>
2025-12-09 07:44:58 +0000

    added csv read

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,75 @@
-#include <iostream>
 #include <Graph.h>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
 #include <ostream>
+#include <sstream>
+#include <string>
+#include <filesystem>
+#include <vector>
 
 using namespace std;
 
+struct edgeInfo {
+    int vertex1;
+    int vertex2;
+    int distance;
+    int cost;
+
+    edgeInfo(int Vertex1, int Vertex2, int Distance, int Cost) {
+        vertex1 = Vertex1;
+        vertex2 = Vertex2;
+        distance = Distance;
+        cost = Cost;
+    }
+};
 
 int main(){
+    vector<Vertex *> vertexes;
+    vector<edgeInfo *> edges;
 
-    Vertex* sfo = new Vertex("San Francisco");
-    Vertex* nyc = new Vertex("New York");
-    Vertex* rio = new Vertex("Rio De Janeiro");
-    Vertex* paris = new Vertex("Paris");
-    Vertex* joburg = new Vertex("Johannesburg");
-    Vertex* moscow = new Vertex("Moscow");
-    Vertex* sydney = new Vertex("Sydney");
-    Vertex* tokyo = new Vertex("Tokyo");
-    Vertex* beijing = new Vertex("Beijing");
-    // Vertex* london = new Vertex("London");
+    //Reading files
+    fstream filePointer_ver;
+    fstream filePointer_edg;
+    filePointer_ver.open("assets/vertices.csv", ios::in);
+    //std::cout << filesystem::current_path() << std::endl; //Debug, I didn't know where the directory started
+    if (filePointer_ver.is_open()) {
+        string theText;
+        while (getline(filePointer_ver, theText)) {
+            vertexes.push_back(new Vertex(theText));
+        }
+    }
+    filePointer_ver.close();
+    filePointer_edg.open("assets/edges.csv", ios::in);
+    if (filePointer_edg.is_open()) {
+        string theText;
+        while (getline(filePointer_edg, theText)) {
+            stringstream line(theText);
+            string current;
+            vector<int> data;
+            while (getline(line,current,',')) {
+                data.push_back(stoi(current));
+            }
+            edges.push_back(new edgeInfo(data[0],data[1],data[2],data[3]));
+        }
+    }
+    filePointer_edg.close();
 
-    std::cout<<"test"<<std::endl;
-
+    // Adding edges and vertexes
     Graph g;
-    g.addVertex(sfo);
-    g.addVertex(nyc);
-    g.addVertex(rio);
-    g.addVertex(paris);
-    g.addVertex(joburg);
-    g.addVertex(moscow);
-    g.addVertex(sydney);
-    g.addVertex(tokyo);
-    g.addVertex(beijing);
+    for (Vertex *location:vertexes) {
+        g.addVertex(location);
+    }
+    for (edgeInfo *edge:edges) {
+        g.addEdge(vertexes[edge->vertex1],vertexes[edge->vertex2],edge->distance);
+    }
 
-    g.addEdge(sfo, nyc, 6);
-    g.addEdge(nyc, rio, 13);
-    g.addEdge(nyc, joburg, 14);
-    g.addEdge(nyc, paris, 7);
-    g.addEdge(nyc, moscow, 15);
+    // Vertex* london = new Vertex("London");
     // g.addEdge(nyc, london, 6);
-    // g.addEdge(london, moscow, 6);
-    g.addEdge(nyc, sydney, 40);
-    g.addEdge(rio, paris, 11);
-    g.addEdge(rio, beijing, 18);
-    g.addEdge(paris, sydney, 17);
-    g.addEdge(joburg, tokyo, 16);
-    g.addEdge(joburg, sydney, 11);
-    g.addEdge(sydney, tokyo, 10);
-    g.addEdge(tokyo, beijing, 3);
-    g.addEdge(beijing, moscow, 8);
+    // g.addEdge(london, moscow, 6););
 
-
-    Waypoint* path = g.ucs(sfo, beijing);
+    Waypoint* path = g.ucs(vertexes[0], vertexes[1]);
 
     if (path){
         cout << "We found a path" << endl;
